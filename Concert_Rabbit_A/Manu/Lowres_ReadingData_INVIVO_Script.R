ae81c5ddb7cd4fd33828d1b4df6eff661311cb38
Niko Vukov <145401264+NikolaiVukov21@users.noreply.github.com>
2026-08-31 14:44:32 -0400

    Add files via upload

--- Concert_Rabbit_A/Manu/Lowres_ReadingData_INVIVO_Script.R
+++ Concert_Rabbit_A/Manu/Lowres_ReadingData_INVIVO_Script.R
@@ -4,11 +4,13 @@ if(!requireNamespace("readxl", quietly = TRUE)) install.packages ("readxl")
 if(!requireNamespace("tidyverse", quietly = TRUE)) install.packages ("tidyverse")
 if(!requireNamespace("purrr", quietly = TRUE)) install.packages ("purrr")
 if(!requireNamespace("rstudioapi", quietly = TRUE)) install.packages ("rstudioapi")
+if(!requireNamespace("lubridate", quietly = TRUE)) install.packages ("lubridate")
 
 library(readxl)
 library(tidyverse)
 library(purrr)
 library(rstudioapi)
+library(lubridate)
 options(scipen=999)
 
 #Log File Name
@@ -58,7 +60,7 @@ has_invivo <-grepl("INVIVO",Folder_Path,ignore.case = TRUE) | grepl("INVINO",Fol
   }
 
   #Moves excel files to the input folder
-  excel_files<-list.files(Folder_Path,pattern="\\.(xlsx?||xlsm||csv)$",full.names = TRUE,recursive = FALSE)
+  excel_files<-list.files(Folder_Path,pattern="\\.(xlsx?|xlsm|csv)$",full.names = TRUE,recursive = FALSE)
   
   #Filters out tempoary lock files
   excel_files<-excel_files[!startsWith(basename(excel_files),"~$")]
@@ -86,22 +88,64 @@ has_invivo <-grepl("INVIVO",Folder_Path,ignore.case = TRUE) | grepl("INVINO",Fol
     }
   }else if (length(excel_files)==0){
       message("No valid excel files were found")
+  }
+  
+  #==========================================================================================  
+  
+  #Clock time helper function
+  normalize_Clock_time<- function(x){
+    if(inherits(x,"POSIXct")){
+      return(format(x,"%H:%M"))
     }
+    
+    x_chr <- trimws(as.character(x))
+    
+    parsed<-suppressWarnings(parse_date_time(
+      toupper(x_chr), 
+      orders=c("HM","IMp","HMW","UMSp")
+    ))
+    
+    is_num_str <- grepl("^[0-9\\.]+$", x_chr) & is.na(parsed) & !is.na(x_chr)
+    
+    if (any(is_num_str)){
+      parsed[is_num_str] <- as.POSIXct(
+        as.numeric(x_chr[is_num_str]) * 86400, 
+        origin = "1899-12-30", 
+        tz = "UTC"
+      )
+    }
+    
+    
+    format(parsed, "%H:%M")
+  }
+  
 
 #==========================================================================================
   process_INVIVO_rabbit_data <- function(data_file, model) {
     
     file_path <- file.path(input_folder, paste0(data_file, ".xlsx"))
     sheets <- excel_sheets(file_path)
+    sheets <- sheets[!grepl("Round|Example|Sheet", sheets, ignore.case = TRUE)]
+    
+    print(basename(data_file))
     
     df_all <-suppressWarnings(suppressMessages(purrr::map_dfr(sheets, function(sheet) {
+  
       read_excel(file_path, range = "A13:BA20", sheet = sheet, trim_ws=T,
                  na = c("", " ","\u00A0", "NA", "N/A", "Not available","unk","did not work", "didnt work", "this does nt seem right", "why????"),
                  .name_repair = function(x) gsub(" ", "_", x)) %>%
-        mutate(ID = sheet, MODEL = model)
-    })
-    ))
-    names(df_all) <- make.names(names(df_all), unique = TRUE)
+        mutate(ID = sheet, MODEL = model) %>% 
+        mutate(Clock_Time=normalize_Clock_time(Clock_Time)) 
+        })))
+    
+    template_names <- names(suppressMessages(read_excel(
+      file_path,range="A13:BA14",sheet= sheets[1],
+      .name_repair = function(x) gsub(" ","_",x)
+    )))
+    
+    exact_metrics_cols<- template_names[3:length(template_names)]
+  
+    #names(df_all) <- make.names(names(df_all), unique = TRUE)
     
     df_domains <- suppressWarnings(suppressMessages(as.data.frame(
       t(read_excel(
@@ -114,86 +158,91 @@ has_invivo <-grepl("INVIVO",Folder_Path,ignore.case = TRUE) | grepl("INVINO",Fol
     ) %>%
       rename(GROUP = V1, UNITS = V2, METRIC = V3) %>% 
       fill(GROUP, .direction = "down") %>%
-      mutate(METRIC = gsub(" ", "_", as.character(METRIC))) %>%
-      mutate(METRIC = gsub("%", "X.", as.character(METRIC))) %>%
-      mutate(METRIC = gsub("R-", "R.", as.character(METRIC))) %>%
-      mutate(METRIC = gsub("K-", "K.", as.character(METRIC))) %>%
-      mutate(METRIC = gsub("IL-", "IL.", as.character(METRIC))) %>%
-      mutate(METRIC = gsub("MCP-", "MCP.", as.character(METRIC))) %>%
+      mutate(METRIC = exact_metrics_cols) %>% 
+
       # Create complex superscript expression
       mutate(
         UNITS = as.character(UNITS),
         UNITS = gsub("10\\^3", "10^3", UNITS),
         UNITS = gsub("10\\^6", "10^6", UNITS),
-        UNITS = gsub("mm\\^3", "mm^3", UNITS)
-      )
-    ))
+        UNITS = gsub("mm\\^3", "mm^3", UNITS),
+        
+
+        UNITS = case_when(
+          grepl("R- | K-", METRIC)    ~ "min",
+          grepl("Angle", METRIC)      ~ "degree",
+          METRIC == "MA" ~ "mm",
+          is.na(UNITS)                ~ "",   
+          TRUE                        ~ UNITS
+        )
+    )))
     
     df_final <- df_all %>% 
       fill(Timepoint, .direction = "down") %>% 
       pivot_longer(MAP:Troponin, values_to = "VALUE", names_to = "METRIC") %>%
       left_join(df_domains, by = "METRIC")  %>% 
-      filter(!is.na(VALUE)) %>%
-      select(-Measurement_.)
+      filter(!is.na(VALUE))
+      
     
     return(df_final)
   }
   
-  datasets_to_process<-list(
-  df_FWB          <- c("CONCERT Y2 Low Res - A-FB", "FWB"),
-  df_LR           <- c("CONCERT Y2 Low Res - A-LR", "LR"),
-  df_SWB          <- c("CONCERT Y2 Low Res - A-SB", "SWB"),
-  df_EMv2.0_WBA   <- c("CONCERT Y2 Low Res - A-EMv2.0-WBA", "EMv2.0_WBA"),
-  df_EMv3.0_WBA   <- c("CONCERT Y2 Low Res - A-EMv3.0-WBA", "EMv3.0_WBA"),
-  df_pHb_SAC      <- c("CONCERT Y2 Low Res - A-pHb-SAC", "pHb_SAC")
-  # df_EMv2.0_SAC   <- c("CONCERT Y2 Low Res - A-EMv2.0-SAC", "EMv2.0_SAC")
-  # df_EMv3.0_SAC   <- c("CONCERT Y2 Low Res - A-EMv3.0-SAC", "EMv3.0_SAC")
-  # df_EMv3.0L_SAC  <- c("CONCERT Y2 Low Res - A-EMv3.0L-SAC", "EMv3.0L_SAC")
-  # df_EMv3.1_SAC   <- c("CONCERT Y2 Low Res - A-EMv3.1-SAC", "EMv3.1_SAC")
-  # df_EMv4.0_SAC   <- c("CONCERT Y2 Low Res - A-EMv4.0-SAC", "EMv4.0_SAC")
-  # df_EMv4.2_SAC   <- c("CONCERT Y2 Low Res - A-EMv4.2-SAC", "EMv4.2_SAC")
-  # df_EMv4.3_SAC   <- c("CONCERT Y2 Low Res - A-EMv4.3-SAC", "EMv4.3_SAC")
-  # df_EMv4.4_SAC   <- c("CONCERT Y2 Low Res - A-EMv4.4-SAC", "EMv4.4_SAC")
-  # df_EMv4.4DL_SAC  <- c("CONCERT Y2 Low Res - A-EMv4.4DL-SAC", "EMv4.4DL_SAC")
-  # df_EMv4.4L_SAC  <- c("CONCERT Y2 Low Res - A-EMv4.4L-SAC", "EMv4.4L_SAC")
-  # df_EMv4.5_SAC   <- c("CONCERT Y2 Low Res - A-EMv4.5-SAC", "EMv4.5_SAC")
-  # df_EMv4.12_SAC  <- c("CONCERT Y2 Low Res - A-EMv4.12-SAC", "EMv4.12_SAC")
-  # df_EMv4.12L_SAC <- c("CONCERT Y2 Low Res - A-EMv4.12L-SAC", "EMv4.12L_SAC")
-  # df_EMv4.14_SAC  <- c("CONCERT Y2 Low Res - A-EMv4.14-SAC", "EMv4.14_SAC")
-  # df_EMv4.14L_SAC <- c("CONCERT Y2 Low Res - A-EMv4.14L-SAC", "EMv4.14L_SAC")
-  # df_EMv4.15_SAC  <- c("CONCERT Y2 Low Res - A-EMv4.15-SAC", "EMv4.15_SAC")
-  # df_EMv4.15DL_SAC <- c("CONCERT Y2 Low Res - A-EMv4.15DL-SAC", "EMv4.15DL_SAC")
-  # df_EMv4.15L_SAC <- c("CONCERT Y2 Low Res - A-EMv4.15L-SAC", "EMv4.15L_SAC")
-  # #df_EMv4.4DL_SAC <- c("CONCERT Y2 Low Res - A-EMv4.4DL-SAC", "EMv4.4DL_SAC")
-  # df_EMv5.0_SAC   <- c("CONCERT Y2 Low Res - A-EMv5.0-SAC", "EMv5.0_SAC")
-  # df_EMv5.0L_SAC  <- c("CONCERT Y2 Low Res - A-EMv5.0L-SAC", "EMv5.0L_SAC")
-  # df_EMv5.2_SAC   <- c("CONCERT Y2 Low Res - A-EMv5.2-SAC", "EMv5.2_SAC")
-  # df_EMv6.0.3sc_SAC   <- c("CONCERT Y2 Low Res - A-EMv6.0.3sc-SAC", "EMv6.0.3sc_SAC")
-  # df_EMv6.0.3L_SAC   <- c("CONCERT Y2 Low Res - A-EMv6.0.3L-SAC", "EMv6.0.3L_SAC")
-  # df_EMv6.0.3rHb_SAC   <- c("CONCERT Y2 Low Res - A-EMv6.0.3rHb-SAC", "EMv6.0.3rHb_SAC")
-  # df_EMv6.1.1_SAC   <- c("CONCERT Y2 Low Res - A-EMv6.1.1-SAC", "EMv6.1.1_SAC")
-  # df_EMv6.1.3_SAC   <- c("CONCERT Y2 Low Res - A-EMv6.1.3-SAC", "EMv6.1.3_SAC")
-  # df_EMv6.3.3c_SAC   <- c("CONCERT Y2 Low Res - A-EMv6.3.3c-SAC", "EMv6.3.3c_SAC")
-  # df_EMv6.5.3_SAC   <- c("CONCERT Y2 Low Res - A-EMv6.5.3-SAC", "EMv6.5.3_SAC")
-  # df_EMv6.6.3_SAC   <- c("CONCERT Y2 Low Res - A-EMv6.6.3-SAC", "EMv6.6.3_SAC")
-  # df_pHb_WBA      <- c("CONCERT Y2 Low Res - A-pHb-WBA", "pHb_WBA")
-  )
-  
-  
-  
+  master_dataset <- tribble(
+  ~sheet_name,                              ~model_name,           ~include,
+  "CONCERT Y2 Low Res - A-FB",              "FWB",                TRUE,
+  "CONCERT Y2 Low Res - A-LR",              "LR",                 TRUE,
+  "CONCERT Y2 Low Res - A-SB",              "SWB",                TRUE,
+  "CONCERT Y2 Low Res - A-pHb-WBA",         "WBA-pHb",            TRUE,
+  "CONCERT Y2 Low Res - A-EMv2.0-WBA",      "EMv2.0_WBA",         TRUE,
+  "CONCERT Y2 Low Res - A-EMv3.0-WBA",      "EMv3.0_WBA",         TRUE,
+  "CONCERT Y2 Low Res - A-EMv2.0-SAC",      "EMv2.0_SAC",         FALSE,
+  "CONCERT Y2 Low Res - A-EMv3.0-SAC",      "EMv3.0_SAC",         FALSE,
+  "CONCERT Y2 Low Res - A-EMv3.0L-SAC",     "EMv3.0L_SAC",        FALSE,  
+  "CONCERT Y2 Low Res - A-EMv3.1-SAC",      "EMv3.1_SAC",         FALSE,  
+  "CONCERT Y2 Low Res - A-EMv4.0-SAC",      "EMv4.0_SAC",         FALSE,
+  "CONCERT Y2 Low Res - A-EMv4.2-SAC",      "EMv4.2_SAC",         FALSE,
+  "CONCERT Y2 Low Res - A-EMv4.3-SAC",      "EMv4.3_SAC",         FALSE,
+  "CONCERT Y2 Low Res - A-EMv4.4-SAC",      "EMv4.4_SAC",         FALSE,
+  "CONCERT Y2 Low Res - A-EMv4.4DL-SAC",    "EMv4.4DL_SAC",       FALSE, 
+  "CONCERT Y2 Low Res - A-EMv4.4L-SAC",     "EMv4.4L_SAC",        FALSE,  
+  "CONCERT Y2 Low Res - A-EMv4.5-SAC",      "EMv4.5_SAC",         FALSE, 
+  "CONCERT Y2 Low Res - A-EMv4.12-SAC",     "EMv4.12_SAC",        FALSE,
+  "CONCERT Y2 Low Res - A-EMv4.12L-SAC",    "EMv4.12L_SAC",       FALSE,
+  "CONCERT Y2 Low Res - A-EMv4.14L-SAC",    "EMv4.14L_SAC",       FALSE,
+  "CONCERT Y2 Low Res - A-EMv4.15DL-SAC",   "EMv4.15DL_SAC",      FALSE,  
+  "CONCERT Y2 Low Res - A-EMv4.15L-SAC",    "EMv4.15L_SAC",       FALSE, 
+  "CONCERT Y2 Low Res - A-EMv4.4DL-SAC",    "EMv4.4DL_SAC",       FALSE, 
+  "CONCERT Y2 Low Res - A-EMv5.0-SAC",      "EMv5.0_SAC",         FALSE, 
+  "CONCERT Y2 Low Res - A-EMv5.0L-SAC",     "EMv5.0L_SAC",        FALSE,
+  "CONCERT Y2 Low Res - A-EMv5.2-SAC",      "EMv5.2_SAC",         FALSE,
+  "CONCERT Y2 Low Res - A-EMv6.0.3sc-SAC",  "EMv6.0.3sc_SAC",     FALSE,
+  "CONCERT Y2 Low Res - A-EMv6.0.3L-SAC",   "EMv6.0.3L_SAC",      FALSE,
+  "CONCERT Y2 Low Res - A-EMv6.0.3rHb-SAC", "EMv6.0.3rHb_SAC",    FALSE,
+  "CONCERT Y2 Low Res - A-EMv6.1.1-SAC",    "EMv6.1.1_SAC",       FALSE,
+  "CONCERT Y2 Low Res - A-EMv6.1.3-SAC",    "EMv6.1.3_SAC",       FALSE,
+  "CONCERT Y2 Low Res - A-EMv6.3.3c-SAC",   "EMv6.3.3c_SAC",      FALSE,
+  "CONCERT Y2 Low Res - A-EMv6.5.3-SAC",    "EMv6.5.3_SAC",       FALSE,
+  "CONCERT Y2 Low Res - A-EMv6.6.3-SAC",    "EMv6.6.3_SAC",       FALSE 
+   )
+  datasets_to_process <- master_dataset %>% 
+    filter(include) %>% 
+    pmap(function(sheet_name, model_name, ...) c(sheet_name, model_name)) %>% 
+    set_names(paste0("df_", master_dataset %>% filter(include) %>% pull(model_name)))
+
+
   df_log <- read_excel(paste0(input_folder,"/", log_filename), 
-                       range = "A1:O250", sheet=2, na = c(""), .name_repair = function(x) gsub(" ", "_", x)) %>%
+                       sheet=2, na = c(""), 
+                       .name_repair = function(x) gsub(" ", "_", x)) %>%
+    select(1:15) %>% 
     rename(INCLUDE = 'In/Excluded', INTERNAL = 'Internal?') %>% 
-    mutate(ID = paste0(Group_Name, " ", ID))
+    mutate(ID = paste0(Group_Name, " ", ID)) 
   
   vec_internal <- df_log  %>% filter(INCLUDE == "Included" & !is.na(INTERNAL))  %>% pull(ID)
   vec_include <- df_log  %>% filter(INCLUDE == "Included" & is.na(INTERNAL))  %>% pull(ID)
   
-  # vec_include <- setdiff(vec_include,c("A-EMv6.6.3-SAC 02","A-LR 17","A-LR 26",
-  #                                      "A-LR 27","A-LR 42","A-LR 43")) 
-  # 
   
   processed_list <- purrr::map(datasets_to_process, ~ process_INVIVO_rabbit_data(.x[1], .x[2]))
+  
   
   df_INVIVO_scenarioA_Master <- bind_rows(processed_list) %>%
     filter(!(is.na(METRIC))) 
